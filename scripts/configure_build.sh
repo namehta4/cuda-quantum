702867fd66877f053db7ca80ea8d6963ae5bc2f0
Neil Mehta <namehta4@DOE8266946.local>
2025-11-10 12:44:37 -0800

    Commented everything out of scripts/config_build.sh and moved to container

--- scripts/configure_build.sh
+++ scripts/configure_build.sh
@@ -23,100 +23,100 @@ export AWS_INSTALL_PREFIX=/usr/local/aws
 
 # [<InstallLocations]
 
-if [ "$1" == "install-cuda" ]; then
-    DISTRIBUTION=${DISTRIBUTION:-rhel8}
-    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
+#if [ "$1" == "install-cuda" ]; then
+#    DISTRIBUTION=${DISTRIBUTION:-rhel8}
+#    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
+#
+## [>CUDAInstall]
+#    CUDA_VERSION=${CUDA_VERSION:-11.8}
+#    CUDA_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cuda/repos
+#    # Go to the url above, set the variables below to a suitable distribution
+#    # and subfolder for your platform, and uncomment the line below.
+#    # DISTRIBUTION=rhel8 CUDA_ARCH_FOLDER=x86_64
+#
+#    dnf config-manager --add-repo "${CUDA_DOWNLOAD_URL}/${DISTRIBUTION}/${CUDA_ARCH_FOLDER}/cuda-${DISTRIBUTION}.repo"
+#    dnf install -y --nobest --setopt=install_weak_deps=False \
+#        cuda-toolkit-$(echo ${CUDA_VERSION} | tr . -)
+#    # custatevec is now linked to `libnvidia-ml.so.1`, which is provided in the NVIDIA driver.
+#    # For build on non-GPU systems, we also need to install the driver. 
+#    dnf install -y --nobest --setopt=install_weak_deps=False nvidia-driver-libs    
+## [<CUDAInstall]
+#fi
 
-# [>CUDAInstall]
-    CUDA_VERSION=${CUDA_VERSION:-12.6}
-    CUDA_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cuda/repos
-    # Go to the url above, set the variables below to a suitable distribution
-    # and subfolder for your platform, and uncomment the line below.
-    # DISTRIBUTION=rhel8 CUDA_ARCH_FOLDER=x86_64
+#if [ "$1" == "install-cudart" ]; then
+#    DISTRIBUTION=${DISTRIBUTION:-rhel8}
+#    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
+#
+## [>CUDARTInstall]
+#    CUDA_VERSION=${CUDA_VERSION:-12.6}
+#    CUDA_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cuda/repos
+#    # Go to the url above, set the variables below to a suitable distribution
+#    # and subfolder for your platform, and uncomment the line below.
+#    # DISTRIBUTION=rhel8 CUDA_ARCH_FOLDER=x86_64
+#
+#    version_suffix=$(echo ${CUDA_VERSION} | tr . -)
+#    dnf config-manager --add-repo "${CUDA_DOWNLOAD_URL}/${DISTRIBUTION}/${CUDA_ARCH_FOLDER}/cuda-${DISTRIBUTION}.repo"
+#    dnf install -y --nobest --setopt=install_weak_deps=False \
+#        cuda-cudart-${version_suffix} \
+#        cuda-nvrtc-${version_suffix} \
+#        libcusolver-${version_suffix} \
+#        libcusparse-${version_suffix} \
+#        libcublas-${version_suffix} \
+#        libcurand-${version_suffix}
+#    if [ $(echo ${CUDA_VERSION} | cut -d . -f1) -gt 11 ]; then 
+#        dnf install -y --nobest --setopt=install_weak_deps=False \
+#            libnvjitlink-${version_suffix}
+#    fi
+## [<CUDARTInstall]
+#fi
 
-    dnf config-manager --add-repo "${CUDA_DOWNLOAD_URL}/${DISTRIBUTION}/${CUDA_ARCH_FOLDER}/cuda-${DISTRIBUTION}.repo"
-    dnf install -y --nobest --setopt=install_weak_deps=False \
-        cuda-toolkit-$(echo ${CUDA_VERSION} | tr . -)
-    # custatevec is now linked to `libnvidia-ml.so.1`, which is provided in the NVIDIA driver.
-    # For build on non-GPU systems, we also need to install the driver. 
-    dnf install -y --nobest --setopt=install_weak_deps=False nvidia-driver-libs    
-# [<CUDAInstall]
-fi
+#if [ "$1" == "install-cuquantum" ]; then
+#    CUDA_VERSION=${CUDA_VERSION:-11.8}
+#    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
+#
+## [>cuQuantumInstall]
+#    CUQUANTUM_VERSION=25.07.0.10
+#    CUQUANTUM_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum
+#
+#    cuquantum_archive=cuquantum-linux-${CUDA_ARCH_FOLDER}-${CUQUANTUM_VERSION}_cuda$(echo ${CUDA_VERSION} | cut -d . -f1)-archive.tar.xz
+#    wget "${CUQUANTUM_DOWNLOAD_URL}/linux-${CUDA_ARCH_FOLDER}/${cuquantum_archive}"
+#    mkdir -p "${CUQUANTUM_INSTALL_PREFIX}" 
+#    tar xf "${cuquantum_archive}" --strip-components 1 -C "${CUQUANTUM_INSTALL_PREFIX}" 
+#    rm -rf "${cuquantum_archive}"
+## [<cuQuantumInstall]
+#fi
 
-if [ "$1" == "install-cudart" ]; then
-    DISTRIBUTION=${DISTRIBUTION:-rhel8}
-    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
+#if [ "$1" == "install-cutensor" ]; then
+#    CUDA_VERSION=${CUDA_VERSION:-12.6}
+#    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
+#
+## [>cuTensorInstall]
+#    CUTENSOR_VERSION=2.3.1.0
+#    CUTENSOR_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cutensor/redist/libcutensor
+#
+#    cutensor_archive=libcutensor-linux-${CUDA_ARCH_FOLDER}-${CUTENSOR_VERSION}_cuda$(echo ${CUDA_VERSION} | cut -d . -f1)-archive.tar.xz
+#    wget "${CUTENSOR_DOWNLOAD_URL}/linux-${CUDA_ARCH_FOLDER}/${cutensor_archive}"
+#    mkdir -p "${CUTENSOR_INSTALL_PREFIX}" && tar xf "${cutensor_archive}" --strip-components 1 -C "${CUTENSOR_INSTALL_PREFIX}"
+#    rm -rf "${cutensor_archive}"
+## [<cuTensorInstall]
+#fi
 
-# [>CUDARTInstall]
-    CUDA_VERSION=${CUDA_VERSION:-12.6}
-    CUDA_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cuda/repos
-    # Go to the url above, set the variables below to a suitable distribution
-    # and subfolder for your platform, and uncomment the line below.
-    # DISTRIBUTION=rhel8 CUDA_ARCH_FOLDER=x86_64
-
-    version_suffix=$(echo ${CUDA_VERSION} | tr . -)
-    dnf config-manager --add-repo "${CUDA_DOWNLOAD_URL}/${DISTRIBUTION}/${CUDA_ARCH_FOLDER}/cuda-${DISTRIBUTION}.repo"
-    dnf install -y --nobest --setopt=install_weak_deps=False \
-        cuda-cudart-${version_suffix} \
-        cuda-nvrtc-${version_suffix} \
-        libcusolver-${version_suffix} \
-        libcusparse-${version_suffix} \
-        libcublas-${version_suffix} \
-        libcurand-${version_suffix}
-    if [ $(echo ${CUDA_VERSION} | cut -d . -f1) -gt 11 ]; then 
-        dnf install -y --nobest --setopt=install_weak_deps=False \
-            libnvjitlink-${version_suffix}
-    fi
-# [<CUDARTInstall]
-fi
-
-if [ "$1" == "install-cuquantum" ]; then
-    CUDA_VERSION=${CUDA_VERSION:-12.6}
-    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
-
-# [>cuQuantumInstall]
-    CUQUANTUM_VERSION=25.09.0.7
-    CUQUANTUM_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum
-
-    cuquantum_archive=cuquantum-linux-${CUDA_ARCH_FOLDER}-${CUQUANTUM_VERSION}_cuda$(echo ${CUDA_VERSION} | cut -d . -f1)-archive.tar.xz
-    wget "${CUQUANTUM_DOWNLOAD_URL}/linux-${CUDA_ARCH_FOLDER}/${cuquantum_archive}"
-    mkdir -p "${CUQUANTUM_INSTALL_PREFIX}" 
-    tar xf "${cuquantum_archive}" --strip-components 1 -C "${CUQUANTUM_INSTALL_PREFIX}" 
-    rm -rf "${cuquantum_archive}"
-# [<cuQuantumInstall]
-fi
-
-if [ "$1" == "install-cutensor" ]; then
-    CUDA_VERSION=${CUDA_VERSION:-12.6}
-    CUDA_ARCH_FOLDER=$([ "$(uname -m)" == "aarch64" ] && echo sbsa || echo x86_64)
-
-# [>cuTensorInstall]
-    CUTENSOR_VERSION=2.3.1.0
-    CUTENSOR_DOWNLOAD_URL=https://developer.download.nvidia.com/compute/cutensor/redist/libcutensor
-
-    cutensor_archive=libcutensor-linux-${CUDA_ARCH_FOLDER}-${CUTENSOR_VERSION}_cuda$(echo ${CUDA_VERSION} | cut -d . -f1)-archive.tar.xz
-    wget "${CUTENSOR_DOWNLOAD_URL}/linux-${CUDA_ARCH_FOLDER}/${cutensor_archive}"
-    mkdir -p "${CUTENSOR_INSTALL_PREFIX}" && tar xf "${cutensor_archive}" --strip-components 1 -C "${CUTENSOR_INSTALL_PREFIX}"
-    rm -rf "${cutensor_archive}"
-# [<cuTensorInstall]
-fi
-
-if [ "$1" == "install-gcc" ]; then
-# [>gccInstall]
-    GCC_VERSION=${GCC_VERSION:-11}
-    dnf install -y --nobest --setopt=install_weak_deps=False \
-        gcc-toolset-${GCC_VERSION}
-    # Enabling the toolchain globally is only needed for debug builds
-    # to ensure that the correct assembler is picked to process debug symbols.
-    enable_script=`find / -path '*gcc*' -path '*'$GCC_VERSIONS'*' -name enable`
-    if [ -n "$enable_script" ]; then
-        . "$enable_script"
-    fi
-# [<gccInstall]
-fi
+#if [ "$1" == "install-gcc" ]; then
+## [>gccInstall]
+#    GCC_VERSION=${GCC_VERSION:-11}
+#    dnf install -y --nobest --setopt=install_weak_deps=False \
+#        gcc-toolset-${GCC_VERSION}
+#    # Enabling the toolchain globally is only needed for debug builds
+#    # to ensure that the correct assembler is picked to process debug symbols.
+#    enable_script=`find / -path '*gcc*' -path '*'$GCC_VERSIONS'*' -name enable`
+#    if [ -n "$enable_script" ]; then
+#        . "$enable_script"
+#    fi
+## [<gccInstall]
+#fi
 
 # [>ToolchainConfiguration]
-export GCC_TOOLCHAIN=/opt/rh/gcc-toolset-11/root/usr/
+export GCC_TOOLCHAIN=/usr
 export CXX="${GCC_TOOLCHAIN}/bin/g++"
 export CC="${GCC_TOOLCHAIN}/bin/gcc"
 export CUDACXX=/usr/local/cuda/bin/nvcc
